01702e327b66c016ad21a4d09b78be29e336e637
greggolang <69986572+greggolang@users.noreply.github.com>
2026-06-02 18:09:56 -0700

    remove 'Don't have an account? Request membership' from login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -66,12 +66,6 @@ export default function Login() {
             {loading ? 'Signing in...' : 'Sign In'}
           </button>
         </form>
-        <p className="text-center text-sm text-gray-500 mt-6">
-          Don't have an account?{' '}
-          <Link to="/register" className="text-lota-700 font-medium hover:underline">
-            Request membership
-          </Link>
-        </p>
       </div>
     </div>
   )
